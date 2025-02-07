843866630aba52352c97bba382e2cfa870831592
garry <tahinjanaharygarry@gmail.com>
2025-02-06 22:04:24 -0900

    first commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,5 @@
+import { ProductCard } from "@ui/components/Product";
+
+export default function Home() {
+  return <ProductCard />
+}
---
+++ packages/ui/src/components/Product/index.tsx
@@ -0,0 +1,4 @@
+export const ProductCard = () => {
+  return <div className="bg-red-500">Product card edited</div>;
+};
+
