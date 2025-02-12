878231d7b662616ba56f7755b8aded7f23fb8480
garry <tahinjanaharygarry@gmail.com>
2025-02-12 01:36:07 -0900

    Refactor AddToCart component layout to improve button and counter alignment

--- apps/web/src/components/AddToCart/index.tsx
+++ apps/web/src/components/AddToCart/index.tsx
@@ -25,13 +25,13 @@ const AddToCart = ({ className = "", setCounter }: Props) => {
   };
 
   return (
-    <div className={`grid grid-cols-3 items-center gap-3 ${className}`}>
-      <div className="bg-light-grayish rounded-[62px] py-3 px-4 flex items-center gap-4">
+    <div className={`grid grid-cols-5 items-center gap-3 ${className}`}>
+      <div className="bg-light-grayish rounded-[62px] col-span-2 py-3 px-4 flex items-center gap-4">
         <Minus onClick={handleMinus} size={20} />
         <span className="paragraph">{count}</span>
         <Plus onClick={handlePlus} size={20} />
       </div>
-      <Button className="!py-3 rounded-[62px] w-full col-span-2">
+      <Button className="!py-3 rounded-[62px] w-full col-span-3">
         Add To Cart
       </Button>
     </div>
