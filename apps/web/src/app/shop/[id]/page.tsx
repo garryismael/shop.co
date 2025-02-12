ec95c866252dc80365cf1ec52d51d04a4222de45
garry <tahinjanaharygarry@gmail.com>
2025-02-12 01:34:16 -0900

    Update query key for product detail fetching to include product ID

--- apps/web/src/app/shop/[id]/page.tsx
+++ apps/web/src/app/shop/[id]/page.tsx
@@ -6,7 +6,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
-    queryKey: ["get-product-detail"],
+    queryKey: ["get-product-detail", id],
     queryFn: () => getProductDetail(id),
   });
   return (
